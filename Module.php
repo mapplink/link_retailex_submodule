b381e524e2a2e9b6ee7d608e2a4a4c6b6ff1c240
gelysis <andreas@gelysis.net>
2016-06-16 11:09:23 +1200

    Added comment and .gitignore

--- Module.php
+++ Module.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * @category Application
+ * @package Application
+ * @author Andreas Gerhards <andreas@lero9.co.nz>
+ * @copyright Copyright (c) 2016 LERO9 Ltd.
+ * @license Commercial - All Rights Reserved
+ */
+
 namespace Retailex;
 
 use Application\MagelinkModule;
