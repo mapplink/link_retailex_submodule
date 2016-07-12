a442efed87e34953efa1378d120c11b8527828a1
gelysis <andreas@gelysis.net>
2016-07-12 14:13:17 +1200

    Added abstract gateway

---
+++ src/Retailex/Gateway/AbstractGateway.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * RetailEx Abstract Gateway
+ * @category RetailEx
+ * @package RetailEx\Gateway
+ * @author Andreas Gerhards <andreas@lero9.co.nz>
+ * @copyright Copyright (c) 2016 LERO9 Ltd.
+ * @license Commercial - All Rights Reserved
+ */
+
+namespace RetailEx\Gateway;
+
+use Magelink\Exception\MagelinkException;
+use Magelink\Exception\GatewayException;
+use Node\AbstractGateway as BaseAbstractGateway;
+
+
+abstract class AbstractGateway extends BaseAbstractGateway
+{
+
+    const GATEWAY_ENTITY = 'generic';
+    const GATEWAY_ENTITY_CODE = 'gty';
+
+
+    /** @var \Entity\Service\EntityConfigService $entityConfigService */
+    protected $entityConfigService = NULL;
+
+    /** @var \RetailEx\Api\Db $this->db */
+    protected $db = NULL;
+    /** @var \RetailEx\Api\Soap $this->soap */
+    protected $soap = NULL;
+
+    /** @var int $lastRetrieveTimestamp */
+    protected $lastRetrieveTimestamp = NULL;
+    /** @var int $newRetrieveTimestamp */
+    protected $newRetrieveTimestamp = NULL;
+
+
+    /**
+     * Initialize the gateway and perform any setup actions required.
+     * @param string $entityType
+     * @throws MagelinkException
+     * @return bool $success
+     */
+    protected function _init($entityType)
+    {
+        $this->db = $this->_node->getApi('db');
+        $this->soap = $this->_node->getApi('soap');
+
+        if (!$this->soap) {
+            throw new GatewayException('SOAP is required for RetailEx '.ucfirst($entityType));
+            $success = FALSE;
+        }else{
+            $this->apiOverlappingSeconds += $this->_node->getConfig('api_overlapping_seconds');
+            $success = TRUE;
+        }
+
+        return $success;
+    }
+
+    /**
+     * ToDo: Implement this instead of the retrieve functionality on all gateways
+     * Retrieve and action all updated records (either from polling, pushed data, or other sources).
+     * @return array $retrieveResults
+     */
+    protected function retrieveEntities() {}
+
+    /**
+     * @return int $this->newRetrieveTimestamp
+     */
+    protected function getNewRetrieveTimestamp()
+    {
+        if ($this->newRetrieveTimestamp === NULL) {
+            $this->newRetrieveTimestamp = $this->getRetrieveTimestamp();
+        }
+
+        return $this->newRetrieveTimestamp;
+    }
+
+    /** @param int $timestamp
+     * @return bool|string $date */
+    protected function convertTimestampToRetailExDateFormat($timestamp)
+    {
+        $deltaInSeconds = intval($this->_node->getConfig('time_delta_'.static::GATEWAY_ENTITY)) * 3600;
+        $date = date('Y-m-d H:i:s', $timestamp + $deltaInSeconds);
+
+        return $date;
+    }
+
+    /** @return bool|string $lastRetrieve */
+    protected function getLastRetrieveDate()
+    {
+        $lastRetrieve = $this->convertTimestampToRetailExDateFormat($this->getLastRetrieveTimestamp());
+        return $lastRetrieve;
+    }
+
+    /** @return bool|int $this->lastRetrieveTimestamp */
+    protected function getLastRetrieveTimestamp()
+    {
+        if ($this->lastRetrieveTimestamp === NULL) {
+            $this->lastRetrieveTimestamp =
+                $this->_nodeService->getTimestamp($this->_nodeEntity->getNodeId(), static::GATEWAY_ENTITY, 'retrieve');
+        }
+
+        return $this->lastRetrieveTimestamp;
+    }
+
+}
