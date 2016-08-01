5f6fcabfaae5946cca5f2af62b919484080a3199
gelysis <andreas@gelysis.net>
2016-08-01 17:03:45 +1200

    Added mapping for product retrieval (and update)

--- src/Retailex/Gateway/ProductGateway.php
+++ src/Retailex/Gateway/ProductGateway.php
@@ -27,6 +27,115 @@ class ProductGateway extends AbstractGateway
     const GATEWAY_ENTITY = 'product';
     const GATEWAY_ENTITY_CODE = 'p';
 
+    protected $attributeMap = array(
+        'ProductId'=>'',
+        'SKU'=>'sku',
+        'Code'=>'',
+        'Description'=>'',
+//        'BrandId'=>'',
+        'SizeId'=>FALSE,
+        'ColourId'=>FALSE,
+//        'SeasonId'=>'',
+        'ProductTypeId'=>'',
+        'Freight'=>'',
+        'Custom3'=>'',
+        'LastUpdated'=>'',
+        'StockAvailable'=>'',
+        'StockOnHand'=>'',
+        'StockOnOrder'=>'',
+        'MatrixProduct'=>'',
+        'ManageStock'=>'',
+        'MasterPOSPrice'=>'price',
+        'RRP'=>'msrp',
+//        'DefaultPrice'=>'price',
+        'DiscountedPrice'=>'special_price',
+        'CustomerDiscountedPrice'=>'',
+        'TaxRate'=>FALSE,
+        'Taxable'=>'',
+        'ChannelId'=>''
+    );
+
+    // ToDo: Move mapping to config
+    /** @var array $this->colourIdMap */
+    protected static $colourById = array(382=>'10K/Diamond', 383=>'10K/Emerald', 384=>'10K/Ruby', 387=>'10K/Silver/Dia',
+        386=>'10K/Silver/Ruby', 479=>'14ct Gold', 385=>'18K', 349=>'9ct Gold', 102=>'Alabaster', 1560=>'Alligator',
+        228=>'Aniseed', 448=>'Anthracite', 121=>'Army', 301=>'Ash', 1510=>'Ashes', 375=>'Ashphalt/Tarseal', 442=>'Ballet',
+        423=>'Basic Black', 1551=>'Basic Grey', 1493=>'Basic Navy', 103=>'Beige', 5=>'Black', 1532=>'Black Angel',
+        307=>'Black Croc', 321=>'Black Diamond', 434=>'Black Emblem', 419=>'Black Eyelet', 1545=>'Black Foil',
+        446=>'Black HAHA', 308=>'Black Magic', 264=>'Black Marle', 1530=>'Black Mix', 303=>'Black Pony', 453=>'Black Putty',
+        342=>'Black Russian', 406=>'Black Scrub', 245=>'Black Sheep', 392=>'Black Spots', 1480=>'Black Stripe',
+        309=>'Black Twill', 1491=>'Black V', 428=>'Black Veil', 1527=>'Black/Almond', 150=>'Black/Black', 274=>'Black/Blue',
+        480=>'Black/Blush', 196=>'Black/Brass', 1520=>'Black/Brown', 183=>'Black/Burg.Gingham', 195=>'Black/Burgundy',
+        184=>'Black/Char.Lurex', 185=>'Black/Charcoal', 62=>'Black/Check', 371=>'Black/Cream', 30=>'Black/Dark',
+        151=>'Black/Fluoro', 278=>'Black/Glow', 284=>'Black/Gold', 1484=>'Black/Gothic', 56=>'Black/Green', 177=>'Black/Grey',
+        43=>'Black/Ink', 29=>'Black/Light', 1488=>'Black/Lurex', 1481=>'Black/Milk', 280=>'Black/Multi', 397=>'Black/Natural',
+        407=>'Black/Navy', 441=>'Black/Nickel', 34=>'Black/Org', 273=>'Black/Pearl', 152=>'Black/Pink', 1487=>'Black/Pink Print',
+        194=>'Black/Pitch', 182=>'Black/Pitch Tartan', 206=>'Black/Plum', 159=>'Black/Pop', 285=>'Black/Poppy',
+        362=>'Black/Print', 318=>'Black/Pumice', 281=>'Black/Purple', 1489=>'Black/Putty', 279=>'Black/Red', 275=>'Black/Silver',
+        473=>'Black/Stripe', 192=>'Black/Tartan', 157=>'Black/Violet', 1483=>'Black/Wallpaper', 33=>'Black/White',
+        283=>'Black/Yellow', 1561=>'Blackadder', 229=>'Blackball', 1544=>'Blackbird', 214=>'Blackboard', 224=>'Blackcheck',
+        233=>'Blacken', 211=>'Blackfelt', 223=>'Blackfelt/Leather', 1492=>'Blackfleece', 429=>'Blacklawn', 259=>'Blackout',
+        212=>'Blackpool', 431=>'Blacksand', 230=>'Blackstretch', 216=>'Blacksuit', 236=>'Blackwash', 225=>'Blackwax',
+        1499=>'Blackwood', 282=>'BlackYellow', 104=>'Bleach', 346=>'Blk Rhodium', 234=>'Blk/Rattlesnake', 343=>'Blood',
+        99=>'Blood Stone', 77=>'Blu Chk', 3=>'Blue', 26=>'Blue Logo', 84=>'Blue Mix', 352=>'Blue Slate', 355=>'Blue Slate/Black',
+        394=>'Blue Spot', 1515=>'Blue Stripes', 25=>'Blue Tartan', 351=>'Blue/White', 222=>'Bluegum/Licorice', 260=>'Bluescreen',
+        105=>'Blush', 106=>'Bonded', 381=>'Bone', 403=>'Bonfire', 320=>'Bottle Prism', 332=>'Bottle Prism/Black', 179=>'Brass',
+        199=>'Brass/Black', 188=>'Brass/Brass', 107=>'Brick', 36=>'Bronze', 7=>'Brown', 28=>'Brown Logo', 270=>'Brown Multi',
+        247=>'Brown Snake', 32=>'Brown Tartan', 244=>'Brownie', 1497=>'Brushblack', 290=>'Buff', 202=>'Burg.Ging/Espresso',
+        145=>'Burgundy', 181=>'Burgundy Gingham', 187=>'Burgundy/Black', 334=>'Burgundy/Dusk', 198=>'Burgundy/Espresso',
+        193=>'Burgundy/Gingham', 329=>'Burgundy/Red', 108=>'Camo', 153=>'Candy', 250=>'Carbon', 52=>'Cargo Green', 109=>'Caviar',
+        1536=>'Chalk', 1490=>'Chambray', 110=>'Charcoal', 364=>'Charcoal Marle', 1552=>'Charred', 143=>'Cherry', 249=>'Chilli',
+        1526=>'Chocolate', 1534=>'Clay Check', 1512=>'Cloud', 370=>'Coal', 1503=>'Coaldust', 254=>'Coffee', 478=>'Concrete',
+        297=>'Coral', 1541=>'Covered', 8=>'Cream', 409=>'Cream/Black', 426=>'Crystal', 217=>'Crystal Black C',
+        218=>'Crystal White C', 241=>'Crystal/Pcorn', 240=>'Crystal/Walnut', 1513=>'Dark', 471=>'Dark Animal', 111=>'Dark Blue',
+        24=>'Dark Brown', 467=>'Dark Dusk', 267=>'Dark Dust', 1537=>'Dark Ink', 1501=>'Dark Mix', 31=>'Dark Tweed',
+        405=>'DarkDust', 252=>'Darl Indigo', 165=>'Dash/Black', 432=>'Decoritif', 266=>'Delft', 41=>'Desert',
+        324=>'Diamond Mix Print', 444=>'Dove', 1496=>'Drill', 291=>'Dusk', 322=>'Dusk/Storm', 469=>'Dust', 295=>'Ebony',
+        1558=>'Eclipse', 97=>'Ecru', 476=>'Ecru/Black', 154=>'Electric', 374=>'Electric/Tarseal', 424=>'Emblem', 1557=>'Emerald',
+        1500=>'Faux', 1498=>'Fine Black', 98=>'Flesh', 314=>'Flint', 142=>'Floral', 1533=>'Flower', 1516=>'Fluro Yellow',
+        413=>'Fog', 215=>'Forest', 395=>'Frostbite', 160=>'Fuchsia/Pop', 243=>'Fudge', 1505=>'Garnet', 411=>'Gingham/Black',
+        348=>'Glass/Silver', 126=>'Glow', 11=>'Gold', 399=>'Gothic', 410=>'Graphic/Yellow', 378=>'Graphite', 2=>'Green',
+        327=>'Green Diamond', 83=>'Green Mix', 477=>'Green Stripe', 173=>'Green/Grey', 333=>'Green/Storm', 462=>'Green/White',
+        1562=>'Greenacres', 6=>'Grey', 246=>'Grey Marle', 340=>'Grey/Black', 167=>'Grey/Blue', 341=>'Grey/Burgundy',
+        475=>'Grey/Green', 48=>'Grey/Ink', 166=>'Grey/Navy', 169=>'Grey/Pink', 168=>'Grey/Purple', 447=>'HAHA', 450=>'HAHA X',
+        155=>'Hands/Black', 239=>'Homemade Black', 176=>'Ice', 1550=>'Iceberg', 93=>'Indigo', 356=>'Indigo/Black', 39=>'Ink',
+        1539=>'Ink Angel', 1538=>'Ink Mix', 415=>'Ink Tattoo', 70=>'Ink/Black', 353=>'Inkpen', 357=>'Inkpen/Black', 112=>'Iron',
+        418=>'Ivory', 175=>'Jade', 427=>'Jet', 242=>'Jetblack', 368=>'Jetsam', 213=>'Kelp', 170=>'Khaki', 1509=>'Khol',
+        414=>'Khol Tattoo', 227=>'Kidblack', 1504=>'Labyrinth', 1547=>'Lacquer', 287=>'Lapis', 226=>'Lateshow', 156=>'Lavender',
+        360=>'Licorice', 354=>'Licorice/Black', 361=>'Licorice/Steel', 472=>'Light Animal', 1522=>'Light Blue', 20=>'Light Grey',
+        114=>'Light Mix', 466=>'Light Orange', 90=>'Lime', 115=>'Logo', 400=>'Lotus', 1543=>'Lurex', 95=>'Mad Wax',
+        257=>'Magenta', 59=>'Mahogany', 21=>'Maroon', 433=>'Marshmellow', 18=>'Matt Black', 19=>'Matt Grey', 376=>'Mauve',
+        373=>'Mauve/Cream', 158=>'Melon', 437=>'Mesh', 96=>'Metal', 189=>'Midnight', 200=>'Midnight/Black', 253=>'Military',
+        144=>'Milk', 417=>'Mist', 277=>'Monster', 15=>'Multi', 289=>'Mushroom', 269=>'Mustard', 92=>'N/A', 1546=>'Natural',
+        398=>'Natural/Black', 17=>'Navy', 53=>'Navy Check', 1514=>'Navy Dots', 436=>'Navy Emblem', 1507=>'Navy Fleece',
+        365=>'Navy Stripe', 174=>'Navy/Black', 337=>'Navy/Bleach', 372=>'Navy/Cream', 248=>'Navy/Ivory', 1518=>'Navy/Stripe',
+        171=>'Navy/White', 336=>'Navy/Yellow', 1555=>'Negative', 366=>'Neo', 54=>'Nickel', 235=>'Noir', 231=>'Nori', 449=>'Nude',
+        1524=>'Off White', 363=>'Oil', 91=>'Olive', 1540=>'Olive Angel', 1531=>'Olive Mix', 298=>'Onyx', 14=>'Orange',
+        23=>'Orange Logo', 306=>'Orange Pony', 465=>'Orange/Pink', 1517=>'Orange/Print', 319=>'Orange/Pumice', 335=>'Orange/Red',
+        345=>'Oxidised Silver', 294=>'Oyster', 161=>'Paint/Black', 401=>'Pale Blue', 1502=>'Pale Mix', 1528=>'Palm',
+        164=>'Papaya', 272=>'Passport', 393=>'Pattern Black', 1521=>'Pattern/Black', 1519=>'Peach', 147=>'Pearl', 66=>'Peat',
+        82=>'Peat/Black', 172=>'Petrol', 210=>'Petrol/Black', 197=>'Petrol/Charcoal', 47=>'Pewter', 1559=>'Phantom', 12=>'Pink',
+        46=>'Pink Mix', 463=>'Pink/Yellow', 293=>'Pirate', 190=>'Pitch', 180=>'Pitch Tartan', 201=>'Pitch Tartan/Black',
+        186=>'Pitch/Black', 208=>'Pitch/Tartan', 178=>'PJ Print', 338=>'Plaid', 27=>'Plum', 207=>'Plum/Black',
+        191=>'Plum/Espresso', 209=>'Plum/Gingham', 1506=>'Polish', 162=>'Pop/White', 148=>'Poppy', 420=>'Porcelain',
+        1556=>'Positive', 313=>'Potion', 122=>'Print', 22=>'Print Mix', 325=>'Prism Mix Print', 57=>'Pumice', 9=>'Purple',
+        445=>'Putty', 454=>'Putty Black', 288=>'Quartz', 1525=>'Rainy Morning', 310=>'Raven', 1=>'Red', 1485=>'Red Check',
+        271=>'Red Multi', 328=>'Red Prism', 232=>'Red Rose', 452=>'Red Slate', 474=>'Red Stripe', 100=>'Red/Black',
+        323=>'Red/Orange', 149=>'Red/White', 350=>'Resin/Petals', 404=>'Rose', 219=>'Rose Red', 316=>'Rosewood', 300=>'Royal',
+        305=>'Royal Pony', 1495=>'Ruby', 377=>'Rust', 292=>'Safari', 1529=>'Sand', 421=>'Saphire', 296=>'Sapphire',
+        220=>'Sateen', 312=>'Satellite', 163=>'Scarlet', 221=>'Scuba', 317=>'Shadow', 10=>'Silver', 425=>'Silver Eyelet',
+        251=>'Silver Marle', 16=>'Silver/ Gold', 388=>'Silver/Emerald', 276=>'Skeleton', 1535=>'Skin', 304=>'Sky Pony',
+        443=>'Slate', 256=>'Smoke', 258=>'Smoke/Black', 255=>'Smoke/Green', 1553=>'Smudge', 116=>'Soap', 380=>'Spec',
+        1494=>'Spotlight', 391=>'Stars', 42=>'Steel', 71=>'Steel/Black', 44=>'Steel/Sil', 347=>'Sterling Silver', 358=>'Stone',
+        359=>'Stone/Black', 326=>'Storm', 299=>'String', 339=>'Stripe', 315=>'Stripe/Black', 389=>'Stripe/Dark Grey',
+        390=>'Stripe/Khaki', 379=>'Syrah', 58=>'T-Shell', 1542=>'Tapestry', 302=>'Tar', 120=>'Tartan', 416=>'Taupe',
+        344=>'Tear', 439=>'Thin Stripe', 262=>'Thunder', 1511=>'Thunderbird', 263=>'Tidal', 94=>'Tortoise',
+        438=>'Triple Stripe', 123=>'Truffle', 1523=>'Turquoise', 117=>'Tweed', 396=>'Uzi', 311=>'Vamp', 367=>'Vanilla',
+        268=>'Vintage Black', 369=>'Violet', 1548=>'Volcanic', 1482=>'Wallpaper', 4=>'White', 1554=>'White Dove',
+        435=>'White Emblem', 422=>'White Eyelet', 440=>'White Veil', 35=>'White/Black', 118=>'White/Blue', 119=>'White/Green',
+        286=>'White/Multi', 265=>'White/Navy', 101=>'White/Red', 430=>'Whitelawn', 237=>'Whitewash', 451=>'X', 13=>'Yellow',
+        1508=>'Yellow Fleece', 464=>'Yellow/Orange', 238=>'Zambesi Black', 1549=>'Zinc');
+
     /**
      * Initialize the gateway and perform any setup actions required.
      * @param string $entityType
@@ -46,6 +155,24 @@ class ProductGateway extends AbstractGateway
         }
 
         return $success;
+    }
+
+    /**
+     * @param $colourId
+     * @return string|NULL $colourString
+     */
+    public static function getColourString($colourId)
+    {
+        return self::getMappedString('colour', $colourId);
+    }
+
+    /**
+     * @param int $colourString
+     * @return int|NULL $colourId
+     */
+    public static function getColourId($colourString)
+    {
+        return self::getMappedId('colour', $colourString);
     }
 
     /**
