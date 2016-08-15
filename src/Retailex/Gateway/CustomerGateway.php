68c1ed86058859514b92e9c0fc4d37ffeaaef9c5
gelysis <andreas@gelysis.net>
2016-08-15 14:46:40 +1200

    Implemented customer gateway with skipping functionality if customer cannot be retrieved from address

--- src/Retailex/Gateway/CustomerGateway.php
+++ src/Retailex/Gateway/CustomerGateway.php
@@ -110,120 +110,147 @@ class CustomerGateway extends AbstractGateway
      */
     public function writeUpdates(Entity $entity, $attributes, $type = \Entity\Update::TYPE_UPDATE)
     {
-        if ($entity->getTypeStr() == 'address') {
-            $entity = $entity->getParent(TRUE);
-            $attributes = array();
-        }
-
         /** @var EntityService $entityService */
         $entityService = $this->getServiceLocator()->get('entityService');
         $nodeId = $this->_node->getNodeId();
 
-        $data = $this->defaultAttributeMapping;
-        $data['Password'] = $this->getRandomPassword();
-        $data['BillEmail'] = $entity->getUniqueId();
-
-        $billingAddress = $entity->resolve('billing_address', 'address');
-        $shippingAddress = $entity->resolve('shipping_address', 'address');
-
-        if (is_null($billingAddress) && !is_null($shippingAddress)) {
-            $billingAddress = $shippingAddress;
-        }elseif (!is_null($billingAddress) && is_null($shippingAddress)) {
-            $shippingAddress = $billingAddress;
-        }
-
-        if (!is_null($billingAddress)) {
-            $street = $billingAddress->getStreet();
-            $data['BillAddress'] = $street;
-//            $data['BillAddress2'] = $street;
-            $data['BillSuburb'] = $billingAddress->getSuburb();
-
-            foreach ($this->billingAttributeMapping as $localCode=>$code) {
-                $data[$localCode] = $billingAddress->getData($code, NULL);
+        if ($entity->getTypeStr() == 'address') {
+            try{
+                $entity = $entity->getParent(TRUE);
+            }catch (\Exception $exception) {
+                $addressUnique = $entity->getUniqueId();
+                $customerLocalId = (int) ltrim('-', strstr($addressUnique, '-'));
+                if ($customerLocalId) {
+                    $entity = $entityService->loadEntityLocal($nodeId, 'customer', 0, $customerLocalId);
+                }else{
+                    $entity = NULL;
+                }
             }
-        }
-        if (!is_null($shippingAddress)) {
-            $name = trim(str_replace('  ', ' ',
-                $shippingAddress->getData('first_name', '')
-                .' '.$shippingAddress->getData('middle_name', '')
-                .' '.$shippingAddress->getData('last_name', '')
-            ));
-            $data['DelName'] = (strlen($name) == 0 ? NULL : $name);
-
-            $street = $shippingAddress->getStreet();
-            $data['DelAddress'] = $street;
-//            $data['DelAddress2'] = $street;
-            $data['DelSuburb'] = $shippingAddress->getSuburb();
-
-            foreach ($this->shippingAttributeMapping as $localCode=>$code) {
-                $data[$localCode] = $shippingAddress->getData($code, NULL);
-            }
+            $attributes = array();
         }
 
-        $deliveryName = '';
-        foreach ($attributes as $attribute) {
-            $value = $entity->getData($attribute);
-
-            // Normal attribute
-            switch ($attribute) {
-                case 'enable_newsletter':
-                    $data['ReceivesNews'] = ($value == 1 ? 1 : 0);
-                    break;
-                case 'first_name':
-                    if (!isset($data['BillFirstName'])) {
-                        $data['BillFirstName'] = $value;
-                    }
-                    if (!isset($data['DelName'])) {
-                        $deliveryName = trim($value.' '.$deliveryName);
-                    }
-                    break;
-                case 'middle_name':
-                    if (!isset($data['DelName'])) {
-                        $deliveryName = trim($deliveryName.' '.$value);
-                    }
-                    break;
-                case 'last_name':
-                    if (!isset($data['BillLastName'])) {
-                        $data['BillLastName'] = $value;
-                    }
-                    if (!isset($data['DelName'])) {
-                        $data['DelName'] = trim($deliveryName.' '.$value);
-                    }
-                    break;
-                case 'date_of_birth':
-                case 'newslettersubscription':
-                    // Ignore these attributes
-                    break;
-                default:
-                    // Warn unsupported attribute
-                    break;
-            }
-        }
-
-        $localId = $entityService->getLocalId($nodeId, $entity);
-        if ($localId) {
-            $type = \Entity\Update::TYPE_UPDATE;
+        if (is_null($entity)) {
+            $success = FALSE;
+            $message = 'Error creating/updating address '.$addressUnique.' on Retailex: '.$exception->getMessage();
+            $this->getServiceLocator()->get('logService')
+                ->log(LogService::LEVEL_ERROR, 'rex_cu_wr_adderr', $message, array());
         }else{
-            $type = \Entity\Update::TYPE_CREATE;
+            $success = TRUE;
+            $data = $this->defaultAttributeMapping;
+            $data['Password'] = $this->getRandomPassword();
+            $data['BillEmail'] = $entity->getUniqueId();
+
+            $billingAddress = $entity->resolve('billing_address', 'address');
+            $shippingAddress = $entity->resolve('shipping_address', 'address');
+
+            if (is_null($billingAddress) && !is_null($shippingAddress)) {
+                $billingAddress = $shippingAddress;
+            }elseif (!is_null($billingAddress) && is_null($shippingAddress)) {
+                $shippingAddress = $billingAddress;
+            }
+
+            if (!is_null($billingAddress)) {
+                $street = $billingAddress->getStreet();
+                $data['BillAddress'] = $street;
+//            $data['BillAddress2'] = $street;
+                $data['BillSuburb'] = $billingAddress->getSuburb();
+
+                foreach ($this->billingAttributeMapping as $localCode=>$code) {
+                    $data[$localCode] = $billingAddress->getData($code, NULL);
+                }
+            }
+            if (!is_null($shippingAddress)) {
+                $name = trim(
+                    str_replace(
+                        '  ',
+                        ' ',
+                        $shippingAddress->getData('first_name', '')
+                        .' '.$shippingAddress->getData('middle_name', '')
+                        .' '.$shippingAddress->getData('last_name', '')
+                    )
+                );
+                $data['DelName'] = (strlen($name) == 0 ? NULL : $name);
+
+                $street = $shippingAddress->getStreet();
+                $data['DelAddress'] = $street;
+//            $data['DelAddress2'] = $street;
+                $data['DelSuburb'] = $shippingAddress->getSuburb();
+
+                foreach ($this->shippingAttributeMapping as $localCode=>$code) {
+                    $data[$localCode] = $shippingAddress->getData($code, NULL);
+                }
+            }
+
+            $deliveryName = '';
+            foreach ($attributes as $attribute) {
+                $value = $entity->getData($attribute);
+
+                // Normal attribute
+                switch ($attribute) {
+                    case 'enable_newsletter':
+                        $data['ReceivesNews'] = ($value == 1 ? 1 : 0);
+                        break;
+                    case 'first_name':
+                        if (!isset($data['BillFirstName'])) {
+                            $data['BillFirstName'] = $value;
+                        }
+                        if (!isset($data['DelName'])) {
+                            $deliveryName = trim($value.' '.$deliveryName);
+                        }
+                        break;
+                    case 'middle_name':
+                        if (!isset($data['DelName'])) {
+                            $deliveryName = trim($deliveryName.' '.$value);
+                        }
+                        break;
+                    case 'last_name':
+                        if (!isset($data['BillLastName'])) {
+                            $data['BillLastName'] = $value;
+                        }
+                        if (!isset($data['DelName'])) {
+                            $data['DelName'] = trim($deliveryName.' '.$value);
+                        }
+                        break;
+                    case 'date_of_birth':
+                    case 'newslettersubscription':
+                        // Ignore these attributes
+                        break;
+                    default:
+                        // Warn unsupported attribute
+                        break;
+                }
+            }
+
+            $localId = $entityService->getLocalId($nodeId, $entity);
+            if ($localId) {
+                $type = \Entity\Update::TYPE_UPDATE;
+            }else{
+                $type = \Entity\Update::TYPE_CREATE;
+            }
+
+            try{
+                $call = 'CustomerCreateUpdate';
+                $data = array('CustomerXML'=>array('Customers'=>array('Customer'=>$data)));
+                $response = $this->soap->call($call, $data);
+
+                $customer = current($response->xpath('//Customer'));
+                $localId = (int) $customer->CustomerId;
+            }catch(\Exception $exception){
+                throw new GatewayException($exception->getMessage(), $exception->getCode(), $exception);
+            }
+
+            if ($type == \Entity\Update::TYPE_CREATE) {
+                if (!$localId) {
+                    $message = 'Error creating customer in Retailex ('.$entity->getUniqueId().'! No local id.';
+                    $this->getServiceLocator()->get('logService')
+                        ->log(LogService::LEVEL_ERROR, 'rex_cu_wr_locerr', $message, array());
+                }else{
+                    $entityService->linkEntity($this->_node->getNodeId(), $entity, $localId);
+                }
+            }
         }
 
-var_dump($data);
-        $data = array('CustomerXML'=>array('Customers'=>array('Customer'=>$data)));
-var_dump($data);
-        $response = $this->soap->call('CustomerCreateUpdate', $data);
-var_dump($response);
-        $customer = current($response->xpath('//Customer'));
-var_dump($customer);
-        foreach ($customer->children() as $data) {
-var_dump($data);
-        }
-die();
-        if($type == \Entity\Update::TYPE_CREATE){
-            if (!$response) {
-                throw new MagelinkException('Error creating customer in Retailex ('.$entity->getUniqueId().'!');
-            }
-            $entityService->linkEntity($this->_node->getNodeId(), $entity, $response['CustomerId']);
-        }
+        return $success;
     }
 
     /**
